a73d7fae3ec388d105b2e8eb9304ecd98f2c1f17
khalifa fattah <khlifa000khlifa@gmail.com>
2025-10-03 14:29:09 +0200

    phone 18

--- js/data.js
+++ js/data.js
@@ -6,7 +6,7 @@ const familyData = [
         members: [
             {
                 id: "person1_1",
-                partnerId: "person1_2"
+                partnerId: "person1_2",
                 name: "احمد عبيد",
                 image: "images/default-avatar",
                 bio: "",
